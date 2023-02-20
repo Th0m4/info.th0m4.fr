783af7d1941202e719fbcff4f83efb8111b3b645
Thomas Staph <tstaphorst@icloud.com>
2023-02-20 18:49:12 +0800

    Update some stuff

--- app/js/script.js
+++ app/js/script.js
@@ -3,8 +3,6 @@ let theme_box = document.getElementById("darklight");
 let darklight_label = document.getElementById("theme_label");
 
 theme_box.addEventListener("click", () => {
-  console.info(theme_box.checked);
-
   if (theme_box.checked == true) {
     darklight_label.innerHTML = "🌚";
     document.documentElement.setAttribute("data-theme", "dark");
